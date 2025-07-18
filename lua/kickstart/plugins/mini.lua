7692028cf33b4900f1326dfa525c6f1585a2e838
xynoan <ncmorva@gmail.com>
2025-07-18 16:24:34 +0800

    refactor: final modularization of configuration

---
+++ lua/kickstart/plugins/mini.lua
@@ -0,0 +1,15 @@
+-- Collection of various small independent plugins/modules
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.ai').setup { n_lines = 500 }
+    require('mini.surround').setup()
+    require('mini.comment').setup()
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+  end,
+}
